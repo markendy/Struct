b5c183c8e83bf71a524b6f78a20b52c3a940a483
markendy <57148996+markendy@users.noreply.github.com>
2019-11-10 20:56:09 +0300

    oxx

--- Struct_labs/10/saod10/saod10/Form1.cs
+++ Struct_labs/10/saod10/saod10/Form1.cs
@@ -42,37 +42,37 @@ namespace saod10
             pictureBox1.BackgroundImage = _fr;
         }       
 
-        private void DrawCarpet(int level, RectangleF carpet)
+        private void DrawCarpet(int lvl, RectangleF ct)
         {
             //проверяем, закончили ли мы построение
-            if (level == 0)
+            if (lvl == 0)
             {
                 //Рисуем прямоугольник
-                _g.FillRectangle(Brushes.Blue, carpet);
+                _g.FillRectangle(Brushes.Blue, ct);
             }
             else
             {
                 // делим прямоугольник на 9 частей
-                var width = carpet.Width / 3f;
-                var height = carpet.Height / 3f;
+                var width = ct.Width / 3f;
+                var height = ct.Height / 3f;
                 // (x1, y1) - координаты левой верхней вершины прямоугольника
                 // от нее будем отсчитывать остальные вершины маленьких прямоугольников
-                var x1 = carpet.Left;
+                var x1 = ct.Left;
                 var x2 = x1 + width;
                 var x3 = x1 + 2f * width;
 
-                var y1 = carpet.Top;
+                var y1 = ct.Top;
                 var y2 = y1 + height;
                 var y3 = y1 + 2f * height;
 
-                DrawCarpet(level - 1, new RectangleF(x1, y1, width, height)); // левый 1(верхний)
-                DrawCarpet(level - 1, new RectangleF(x2, y1, width, height)); // средний 1
-                DrawCarpet(level - 1, new RectangleF(x3, y1, width, height)); // правый 1
-                DrawCarpet(level - 1, new RectangleF(x1, y2, width, height)); // левый 2
-                DrawCarpet(level - 1, new RectangleF(x3, y2, width, height)); // правый 2
-                DrawCarpet(level - 1, new RectangleF(x1, y3, width, height)); // левый 3
-                DrawCarpet(level - 1, new RectangleF(x2, y3, width, height)); // средний 3
-                DrawCarpet(level - 1, new RectangleF(x3, y3, width, height)); // правый 3
+                DrawCarpet(lvl - 1, new RectangleF(x1, y1, width, height)); // левый 1(верхний)
+                DrawCarpet(lvl - 1, new RectangleF(x2, y1, width, height)); // средний 1
+                DrawCarpet(lvl - 1, new RectangleF(x3, y1, width, height)); // правый 1
+                DrawCarpet(lvl - 1, new RectangleF(x1, y2, width, height)); // левый 2
+                DrawCarpet(lvl - 1, new RectangleF(x3, y2, width, height)); // правый 2
+                DrawCarpet(lvl - 1, new RectangleF(x1, y3, width, height)); // левый 3
+                DrawCarpet(lvl - 1, new RectangleF(x2, y3, width, height)); // средний 3
+                DrawCarpet(lvl - 1, new RectangleF(x3, y3, width, height)); // правый 3
             }
         }
     }
